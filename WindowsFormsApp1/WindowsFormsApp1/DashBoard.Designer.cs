cd1a6c2832c0114143f3206f8d5698ef85e7c75e
dutdut <diepchandat1999@gmail.com>
2021-06-13 22:40:19 +0700

    Dashboard shity

---
+++ WindowsFormsApp1/WindowsFormsApp1/DashBoard.Designer.cs
@@ -0,0 +1,227 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class DashBoard
+    {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea7 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend7 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series7 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea8 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend8 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series8 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.btnShow = new System.Windows.Forms.Button();
+            this.chart2 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
+            this.button3 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.chart2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // chart1
+            // 
+            chartArea7.Name = "ChartArea1";
+            this.chart1.ChartAreas.Add(chartArea7);
+            legend7.Name = "Legend1";
+            this.chart1.Legends.Add(legend7);
+            this.chart1.Location = new System.Drawing.Point(370, 22);
+            this.chart1.Name = "chart1";
+            series7.BorderDashStyle = System.Windows.Forms.DataVisualization.Charting.ChartDashStyle.NotSet;
+            series7.BorderWidth = 0;
+            series7.ChartArea = "ChartArea1";
+            series7.IsValueShownAsLabel = true;
+            series7.Legend = "Legend1";
+            series7.Name = "Series1";
+            this.chart1.Series.Add(series7);
+            this.chart1.Size = new System.Drawing.Size(654, 306);
+            this.chart1.TabIndex = 0;
+            this.chart1.Text = "chart1";
+            // 
+            // btnShow
+            // 
+            this.btnShow.Location = new System.Drawing.Point(1030, 79);
+            this.btnShow.Name = "btnShow";
+            this.btnShow.Size = new System.Drawing.Size(75, 23);
+            this.btnShow.TabIndex = 1;
+            this.btnShow.Text = "Show";
+            this.btnShow.UseVisualStyleBackColor = true;
+            this.btnShow.Click += new System.EventHandler(this.btnShow_Click);
+            // 
+            // chart2
+            // 
+            chartArea8.Name = "ChartArea1";
+            this.chart2.ChartAreas.Add(chartArea8);
+            legend8.Name = "Legend1";
+            this.chart2.Legends.Add(legend8);
+            this.chart2.Location = new System.Drawing.Point(557, 334);
+            this.chart2.Name = "chart2";
+            series8.ChartArea = "ChartArea1";
+            series8.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Doughnut;
+            series8.IsValueShownAsLabel = true;
+            series8.Legend = "Legend1";
+            series8.Name = "Series1";
+            this.chart2.Series.Add(series8);
+            this.chart2.Size = new System.Drawing.Size(212, 185);
+            this.chart2.TabIndex = 2;
+            this.chart2.Text = "chart2";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(240, 377);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F);
+            this.label1.Location = new System.Drawing.Point(29, 79);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(64, 25);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "label1";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F);
+            this.label2.Location = new System.Drawing.Point(29, 152);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(64, 25);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "label2";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F);
+            this.label3.Location = new System.Drawing.Point(29, 220);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(64, 25);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "label3";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(34, 274);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 7;
+            this.button2.Text = "button2";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "Tháng 1",
+            "Tháng 2",
+            "Tháng 3",
+            "Tháng 4",
+            "Tháng 5",
+            "Tháng 6",
+            "Tháng 7",
+            "Tháng 8",
+            "Tháng 9",
+            "Tháng 10",
+            "Tháng 11",
+            "Tháng 12",
+            "January"});
+            this.comboBox1.Location = new System.Drawing.Point(34, 336);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(121, 21);
+            this.comboBox1.TabIndex = 9;
+            // 
+            // dateTimePicker1
+            // 
+            this.dateTimePicker1.Location = new System.Drawing.Point(34, 380);
+            this.dateTimePicker1.Name = "dateTimePicker1";
+            this.dateTimePicker1.Size = new System.Drawing.Size(200, 20);
+            this.dateTimePicker1.TabIndex = 10;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(161, 336);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 11;
+            this.button3.Text = "button3";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // DashBoard
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.dateTimePicker1);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.chart2);
+            this.Controls.Add(this.btnShow);
+            this.Controls.Add(this.chart1);
+            this.Name = "DashBoard";
+            this.Size = new System.Drawing.Size(1221, 539);
+            this.Load += new System.EventHandler(this.DashBoard_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.chart2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart1;
+        private System.Windows.Forms.Button btnShow;
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.DateTimePicker dateTimePicker1;
+        private System.Windows.Forms.Button button3;
+    }
+}
---
+++ WindowsFormsApp1/WindowsFormsApp1/DashBoard.cs
@@ -0,0 +1,170 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace WindowsFormsApp1
+{
+    public partial class DashBoard : UserControl
+    {
+        public DashBoard()
+        {
+            InitializeComponent();
+        }
+
+        public void soluongtrongngay(string ngay)
+        {
+            dbConnection db = new dbConnection();
+            db.openConnection();
+            string sql = @"SELECT Count(*) 
+                            FROM HOADON
+                            WHERE  THOIGIAN >= '" + ngay + " 0:00:00' AND  THOIGIAN <= '" + ngay + " 23:59:59'";
+            Int32 ID = Convert.ToInt32(db.sqlExecuteScalar(sql));
+
+
+            string sql2 = @"SELECT SUM(HOADON.TONGTIEN)
+                            FROM HOADON
+                            WHERE  THOIGIAN >= '" + ngay + " 0:00:00' AND  THOIGIAN <= '" + ngay + " 23:59:59'";
+            Int32 TONGTIEN = Convert.ToInt32(db.sqlExecuteScalar(sql2));
+
+            label1.Text = "Tổng bán được: "+ID + "bill";
+            label2.Text = "Bán được hiện tại là: " +TONGTIEN.ToString("C")+ "";
+            label3.Text = ngay+"";
+
+
+
+            db.disPose();
+
+        }
+
+
+        public void Chart(string NgayThangNam)
+        {
+            chart1.Series[0].Points.Clear();
+            chart2.Series[0].Points.Clear();
+            dbConnection db = new dbConnection();
+            List<string> Query = new List<string>();
+          
+            Query.Add(@"SELECT  Count(*) AS '7-12'
+                        FROM HOADON
+                        WHERE THOIGIAN >= '" + NgayThangNam + " 7:00:00' AND  THOIGIAN <= '" + NgayThangNam + " 12:0:0'");
+            Query.Add(@"SELECT  Count(*) AS '12-19'
+                        FROM HOADON
+                        WHERE THOIGIAN >= '" + NgayThangNam + " 12:00:00' AND  THOIGIAN <= '" + NgayThangNam + " 19:0:0'");
+            Query.Add(@"SELECT  Count(*) AS '19-0'
+                        FROM HOADON
+                        WHERE THOIGIAN >= '" + NgayThangNam + " 19:00:00' AND  THOIGIAN <= '" + NgayThangNam + " 23:59:59'");
+
+            int i = 0;
+            db.openConnection();
+            foreach (string values in Query)
+            {
+                SqlCommand cmd = new SqlCommand(values, db.Con);
+
+                SqlDataReader reader = cmd.ExecuteReader();
+                string title = @"";
+                switch (i)
+                {
+                    case 0:
+                        title = "7-12";
+                        break;
+                    case 1:
+                        title = "12-19";
+                        break;
+                    case 2:
+                        title = "19-0";
+                        break;
+                }
+                while (reader.Read())
+                {
+                    chart1.Series[0].Points.AddXY(title, reader[0].ToString());
+                    chart2.Series[0].Points.AddXY(title, reader[0].ToString());
+                }
+                reader.Close();
+                i++;
+
+            }
+            db.closeConnection();
+        }
+
+
+
+        private void btnShow_Click(object sender, EventArgs e)
+        {
+
+            string NgayThangNam = DateTime.Now.ToString("yyyy/MM/dd");
+            Chart(NgayThangNam);
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            soluongtrongngay(DateTime.Now.ToString("yyyy/MM/dd"));
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                Chart(dateTimePicker1.Text.ToString());
+                soluongtrongngay(dateTimePicker1.Text.ToString());
+            }
+            catch { }
+        }
+
+        private void DashBoard_Load(object sender, EventArgs e)
+        {
+            dateTimePicker1.Format = DateTimePickerFormat.Custom;
+            dateTimePicker1.CustomFormat = "yyyy/MM/dd";
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            
+            IDictionary<string, int> monthsDictionary = new Dictionary<string, int>();
+            monthsDictionary.Add("Tháng 1", 1);
+            monthsDictionary.Add("Tháng 2", 2);
+            monthsDictionary.Add("Tháng 3", 3);
+            monthsDictionary.Add("Tháng 4", 4);
+            monthsDictionary.Add("Tháng 5", 5);
+            monthsDictionary.Add("Tháng 6", 6);
+            monthsDictionary.Add("Tháng 7", 7);
+            monthsDictionary.Add("Tháng 8", 8);
+            monthsDictionary.Add("Tháng 9", 9);
+            monthsDictionary.Add("Tháng 10", 10);
+            monthsDictionary.Add("Tháng 11", 11);
+            monthsDictionary.Add("Tháng 12", 12);
+            int month = 0;
+            month = monthsDictionary[comboBox1.SelectedItem.ToString()] ;
+
+            
+
+            string Query = @"SELECT  Count(*) AS BILL
+                            FROM HOADON
+                            WHERE MONTH(THOIGIAN) = " + month;
+
+            dbConnection db = new dbConnection();
+            db.openConnection();
+            SqlCommand sqlc = new SqlCommand(Query, db.Con);
+
+            SqlDataReader reader = sqlc.ExecuteReader();
+                
+            while (reader.Read())
+            {
+                chart2.Series[0].Points.AddXY(comboBox1.SelectedItem.ToString(), reader[0].ToString());
+               // chart2.Series[0].Points.AddXY(comboBox1.SelectedItem.ToString(), reader[0].ToString());
+            }
+            reader.Close();
+
+            db.closeConnection();
+
+
+        }
+    }
+}
